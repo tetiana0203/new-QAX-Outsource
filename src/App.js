30365a6734efa4020906b505b7a8c4b51664629b
tatiana0203 <putchenko.t.a@gmail.com>
2022-09-24 06:01:32 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
+
+function App() {
+  return (
+    <div className="App">
+       <Header title="My header" subtitle="subtitle2" />
+       <Main />
+       <Footer  />
+       < />
+    </div>
+  );
+}
+
+export default App;
